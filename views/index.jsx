90af6f0114cb0a28e6ab216d59328896d7e583e8
Danyal Imran <dan.imran97@gmail.com>
2024-08-06 01:05:20 -0400

    converting ejs to jsx

---
+++ views/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from './components/Main'
+
+export default function Index(props) {
+return (
+    <Main>
+        <main className="container">
+            <div className="row justify-content-around mt-5">
+                <a href="/login" className="col-3 btn btn-primary">Login</a>
+                <a href="/signup" className="col-3 btn btn-primary">Signup</a>
+            </div>
+        </main>
+    </Main>
+)
+}
